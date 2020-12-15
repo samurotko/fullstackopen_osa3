2c9d1c5432574829be9ee25a1b0810b8665047a2
samurotko <samu.rotko@aalto.fi>
2020-12-15 14:49:51 +0200

    vs test commit

--- index.js
+++ index.js
@@ -99,10 +99,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
   
     response.status(204).end()
   })
-  
-  
 
-  
   const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
