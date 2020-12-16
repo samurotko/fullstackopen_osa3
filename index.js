a3670a514f157aee3f08521149273d51fcad228a
samurotko <samu.rotko@aalto.fi>
2020-12-16 15:00:01 +0200

    uibuild

--- index.js
+++ index.js
@@ -20,39 +20,42 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 
 
 
-  // let persons = [
-  //   { 
-  //     name: "Arto Hellas", 
-  //     number: "040-123456",
-  //     id: 1
-  //   },
-  //   { 
-  //     name: "Ada Lovelace", 
-  //     number: "39-44-5323523",
-  //     id: 2
-  //   },
-  //   { 
-  //     name: "Dan Abramov", 
-  //     number: "12-43-234345",
-  //     id: 3
-  //   },
-  //   { 
-  //     name: "Mary Poppendieck", 
-  //     number: "39-23-6423122",
-  //     id: 4
-  //   }
-  // ]
+  let persons = [
+    { 
+      name: "Arto Hellas", 
+      number: "040-123456",
+      id: 1
+    },
+    { 
+      name: "Ada Lovelace", 
+      number: "39-44-5323523",
+      id: 2
+    },
+    { 
+      name: "Dan Abramov", 
+      number: "12-43-234345",
+      id: 3
+    },
+    { 
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122",
+      id: 4
+    }
+  ]
 
 
   app.get('/', (req, res) => {
-    res.send('<h1>fuck!</h1>')
+    console.log("/")
+    res.send('<h1>fuckkkk!</h1>')
   })
   
   app.get('/api/persons', (req, res) => {
-    Person.find({}).then(persons => {
-      res.json(persons)
-    })
-    
+    // Person.find({}).then(persons => {
+    //   res.json(persons)
+      
+    // })
+    console.log("api/persons")
+    res.json(persons)
   })
 
   app.get('/api/persons/:id', (request, response) => {
